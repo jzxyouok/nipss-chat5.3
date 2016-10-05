a373f3d7f5a12e6271e988164474d5d887835293
nanichang <nanipaul68@gmail.com>
2016-10-06 00:38:58 +0100

    created message controller with functions

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Repositories\Message\MessageContract;
 use App\Http\Requests;
 
 class MessageController extends Controller
 {
-    public function sendMessages() {
-
-    	
-
+    protected $repo;
+    
+    public function __construct(MessageContract $messageContract){
+        $this->repo = $messageContract;
+    }
+    
+    public function postMessage(Request $request){
+         if($this->repo->create($request)){
+             //this redirect is to return view with array of all message sent
+           return redirect()->back(); //->with('') ;   
+        }
     }
 }
