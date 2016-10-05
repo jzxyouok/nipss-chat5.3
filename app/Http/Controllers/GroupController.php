fc59f369d775192379eb5b68e7b7cec33881b389
nanichang <nanipaul68@gmail.com>
2016-10-06 00:40:23 +0100

    added message contract to group

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -5,29 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\Group\GroupContract;
 use App\Repositories\Participant\ParticipantContract;
+use App\Repositories\Message\MessageContract;
 use App\Http\Requests;
 
 use App\Participant;
-use APp\Group;
+use App\Group;
+use App\Message;
+use Auth;
 
 class GroupController extends Controller
 {
     
     protected $repo;
     protected $participantRepo;
+    protected $messageRepo;
     
-    public function __construct(GroupContract $groupContract, ParticipantContract $participantContract){
+    public function __construct(GroupContract $groupContract, ParticipantContract $participantContract, Message $messageContract){
        $this->repo = $groupContract;
        $this->participantRepo = $participantContract;
+       $this->messageRepo = $messageContract;
         
     }
-
+    // Show an Group with its messages
     public function index(){
+    	if(!Auth::check()){
+    		return redirect()->route('login');
+    	}
     	$participants = $this->participantRepo->findAll();
-    	return view('groups.index')->with('participants', $participants);
+    	$messages = $this->messageRepo->id;
+
+    	return view('groups.index', ['participants' => $participants, 'messages' => $messages]);
     }
 
-    public function fuckyou(){
-    	return view('try');
-    }
 }
